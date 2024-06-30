496f21ad8949b3116e7d6b8811adb2b5c74de66f
Akhand Pratap Singh <83303925+AkhandPratapSingh11@users.noreply.github.com>
2024-06-30 12:40:26 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,51 +1,44 @@
-import re
 import pandas as pd
+import re
 
-def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+def preprocess(data, time_format):
+    if time_format == "12-hour":
+        pattern = r'\d{2}/\d{2}/\d{2}, \d{1,2}:\d{2}\s[ap]m - '  # Matches date and time
+        date_format = '%d/%m/%y, %I:%M %p - '
+    else:  # 24-hour format
+        pattern = r'\d{2}/\d{2}/\d{2}, \d{1,2}:\d{2} - '  # Matches date and time
+        date_format = '%d/%m/%y, %H:%M - '
+    
+    messages = re.split(pattern, data)[1:]  # Split data into messages
+    dates = re.findall(pattern, data)  # Find all dates and times
 
-    messages = re.split(pattern, data)[1:]
-    dates = re.findall(pattern, data)
-
-    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
-
-    df.rename(columns={'message_date': 'date'}, inplace=True)
+    df = pd.DataFrame({'date_time': dates, 'message': messages})
+    df['date_time'] = pd.to_datetime(df['date_time'], format=date_format)
+    df.rename(columns={'date_time': 'date'}, inplace=True)
 
     users = []
-    messages = []
-    for message in df['user_message']:
+    msgs = []
+    for message in df['message']:
         entry = re.split('([\w\W]+?):\s', message)
-        if entry[1:]:  # user name
+        if entry[1:]:
             users.append(entry[1])
-            messages.append(" ".join(entry[2:]))
+            msgs.append(" ".join(entry[2:]))
         else:
             users.append('group_notification')
-            messages.append(entry[0])
+            msgs.append(entry[0])
 
     df['user'] = users
-    df['message'] = messages
-    df.drop(columns=['user_message'], inplace=True)
+    df['message'] = msgs
+    df.drop(columns=['message'], inplace=False)  # Ensure 'message' column is not dropped
 
-    df['only_date'] = df['date'].dt.date
+    # Extracting additional columns for analysis
     df['year'] = df['date'].dt.year
-    df['month_num'] = df['date'].dt.month
     df['month'] = df['date'].dt.month_name()
+    df['month_num'] = df['date'].dt.month
     df['day'] = df['date'].dt.day
     df['day_name'] = df['date'].dt.day_name()
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
-
-    period = []
-    for hour in df[['day_name', 'hour']]['hour']:
-        if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
-        elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
-        else:
-            period.append(str(hour) + "-" + str(hour + 1))
-
-    df['period'] = period
-
+    df['only_date'] = df['date'].dt.date
+    
     return df
